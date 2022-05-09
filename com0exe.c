9908a579e6da3f6fba4fc86705d05fbac7f5d375
Peter Szabo <pts@fazekas.hu>
2022-05-09 03:25:52 +0200

    reduced buf memory usage by 48 KiB, by interleaving the 16 KiB reads with the writes

--- com0exe.c
+++ com0exe.c
@@ -13,8 +13,12 @@
   #define O_RDWR 2
   #define O_CREAT 0100
   #define O_TRUNC 01000
+  #define SEEK_SET 0
+  #define SEEK_CUR 1
+  #define SEEK_END 2
   typedef unsigned long size_t;
   typedef long ssize_t;
+  typedef long off_t;
   typedef unsigned short uint16_t;
   extern size_t strlen(const char *__s)  __attribute__((__nothrow__ , __leaf__)) __attribute__((__pure__)) __attribute__((__nonnull__(1)));
   extern void _exit(int __status) __attribute__((__noreturn__));
@@ -25,6 +29,7 @@
   extern ssize_t read(int __fd, void *__buf, size_t __nbytes) ;
   extern ssize_t write(int __fd, const void *__buf, size_t __n);
   extern int close(int __fd);
+  extern __off_t lseek(int __fd, __off_t __offset, int __whence) __attribute__((__nothrow__ , __leaf__));
   #define strerror(errno) "I/O error"
   #define exit(x) _exit(x)
   #define NULL ((void*)0)
@@ -114,7 +119,7 @@ static void change_ext(char *fn, const char *ext) {
 /* Same as in kvikdos. */
 #define MAX_DOS_COM_SIZE 0xfef0
 
-static char buf[MAX_DOS_COM_SIZE + 1 + 11];
+static char buf[16384 + 11];
 
 static uint16_t exehdr[16];  /* 32 bytes, including the last 8 trailing bytes for output_signature or variant_27_intro. */
 
@@ -167,7 +172,7 @@ int main(int argc, char **argv) {
   char noret = 0, variant;
   char *infn, *outfn;
   int infd, outfd, got;
-  uint16_t com_size, exehdr_size, entry;
+  uint16_t com_size, com_size_with_trailer, com_size_read, exehdr_size, entry;
   char *com_start;
 
   (void)argc;
@@ -202,41 +207,35 @@ int main(int argc, char **argv) {
   infn = *argv++;
   outfn = argv[0] ? *argv++ : NULL;
 
-  { char *p = buf;
+  {
     if ((infd = open(infn, O_RDONLY | O_BINARY)) < 0) {
       write_str3(STDERR_FILENO, "fatal: error opening input file: ", infn, ": ");
+     error_errno:
       write_str3(STDERR_FILENO, strerror(errno), "\n", "");
       exit(2);
     }
-    p = buf;
     if ((got = read(infd, buf, 16384)) < 0) { read_error:
       write_str3(STDERR_FILENO, "fatal: error reading input file: ", infn, ": ");
-      write_str3(STDERR_FILENO, strerror(errno), "\n", "");
-      exit(2);
+      goto error_errno;
     }
-    p += got;
-    if (got == 16384) {
-      if ((got = read(infd, p, 16384)) < 0) goto read_error;
-      p += got;
-      if (got == 16384) {
-        if ((got = read(infd, p, 16384)) < 0) goto read_error;
-        p += got;
-        if (got == 16384) {
-          const int need = MAX_DOS_COM_SIZE + 1 - (p - buf);
-          { struct SA { int StaticAssert_MaxSize : MAX_DOS_COM_SIZE > 3 * 16384U; }; }
-          if ((got = read(infd, p, need)) < 0) goto read_error;
-          p += got;
-          if (got == need) {
-            write_str3(STDERR_FILENO, "fatal: .com program too long: ", infn, ": ");
-            write_u10(STDERR_FILENO, (unsigned)(p - buf));
-            (void)!write(STDERR_FILENO, "\n", 1);
-            exit(2);
-          }
-        }
+    if (got == 16384) {  /* !! test with smaller. */
+      long file_size;
+      if ((file_size = lseek(infd, 0, SEEK_END)) < 0) { seek_error:
+        write_str3(STDERR_FILENO, "fatal: error seeking input file: ", infn, ": ");
+        goto error_errno;
       }
+      if (file_size > MAX_DOS_COM_SIZE) {
+        write_str3(STDERR_FILENO, "fatal: .com program too long: ", infn, ": ");
+        write_u10(STDERR_FILENO, (unsigned)file_size);
+        (void)!write(STDERR_FILENO, "\n", 1);
+        exit(2);
+      }
+      if (lseek(infd, 16384, SEEK_SET) != 16384) goto seek_error;
+      com_size = (unsigned)file_size;
+      com_size_read = 16384;
+    } else {
+      com_size = com_size_read = got;
     }
-    close(infd);
-    com_size = p - buf;
   }
   if (com_size >= 10 && memcmp(buf, variant_24_com_prefix, sizeof(variant_24_com_prefix)) == 0) {
     variant = 24;
@@ -250,20 +249,21 @@ int main(int argc, char **argv) {
   { const uint16_t delta = (variant >= 32) ? 0 : variant - 16;
     com_start = buf + delta;
     com_size -= delta;
+    com_size_read -= delta;
   }
   if (variant == 43) {
     entry = com_size;
-    memcpy(com_start + com_size, variant_43_trailer, sizeof(variant_43_trailer));
-    com_size += sizeof(variant_43_trailer);
+    com_size_with_trailer = com_size + sizeof(variant_43_trailer);
   } else {
     entry = variant & 8;
+    com_size_with_trailer = com_size;
     if (variant == 27) {
       memcpy((char*)exehdr + 24, variant_27_intro, sizeof(variant_27_intro));
     }
   }
   if (variant < 32) {
     exehdr_size = variant;
-    for (; exehdr_size + com_size < 28; com_start[com_size++] = '\0') {}
+    for (; exehdr_size + com_size < 28; com_start[com_size++] = '\0', ++com_size_read, ++com_size_with_trailer) {}
   } else {
     exehdr_size = 32;
     memcpy((char*)exehdr + 24, output_signature, sizeof(output_signature));
@@ -275,11 +275,11 @@ int main(int argc, char **argv) {
 
   { uint16_t *eh = exehdr;
     const uint16_t exehdr_para = variant >> 4;  /* = (variant >= 32) ? 2 : 1; */
-    const uint16_t imagex_size = exehdr_size + com_size;
+    const uint16_t imagex_size = exehdr_size + com_size_with_trailer;
     const uint16_t imagex_size_lastsize = imagex_size & 0x1ff;
     const uint16_t imagex_size_nblocks = (imagex_size >> 9) + (imagex_size_lastsize ? 1 : 0);  /* No uint16_t overflow. */
     const uint16_t imagea_para = (imagex_size_nblocks << 5) - exehdr_para;
-    if ((exehdr_size & ~15) + com_size > MAX_DOS_COM_SIZE) {
+    if ((exehdr_size & ~15) + com_size_with_trailer > MAX_DOS_COM_SIZE) {
       write_str3(STDERR_FILENO, "fatal: .exe program would be too long: ", infn, "\n");
       exit(2);
     }
@@ -315,25 +315,33 @@ int main(int argc, char **argv) {
     outfn = infn;  /* Owned by argv. */
     change_ext(outfn, "exe");
   }
-  { const char *p;
-    uint16_t remaining;
-    if ((outfd = open(outfn, O_WRONLY | O_BINARY | O_CREAT | O_TRUNC, 0644)) < 0) {
-      write_str3(STDERR_FILENO, "fatal: error opening output file: ", outfn, ": ");
-      write_str3(STDERR_FILENO, strerror(errno), "\n", "");
-      exit(2);
-    }
-    if ((int)write(outfd, exehdr, exehdr_size) != (int)exehdr_size) { write_error:
-      write_str3(STDERR_FILENO, "fatal: error writing output file: ", outfn, ": ");
-      write_str3(STDERR_FILENO, strerror(errno), "\n", "");
-      exit(2);
-    }
-    p = com_start; remaining = com_size;
-    while (remaining > 0) {
-      const int attempt = (remaining > 16384) ? 16384 : remaining;
-      if (write(outfd, p, attempt) != attempt) goto write_error;
-      p += attempt; remaining -= attempt;
-    }
-    close(outfd);
+  if ((outfd = open(outfn, O_WRONLY | O_BINARY | O_CREAT | O_TRUNC, 0644)) < 0) {
+    write_str3(STDERR_FILENO, "fatal: error opening output file: ", outfn, ": ");
+    goto error_errno;
   }
+  if ((int)write(outfd, exehdr, exehdr_size) != (int)exehdr_size) { write_error:
+    write_str3(STDERR_FILENO, "fatal: error writing output file: ", outfn, ": ");
+    goto error_errno;
+  }
+  if (com_size == com_size_read && variant == 43) {
+    memcpy(com_start + com_size, variant_43_trailer, sizeof(variant_43_trailer));
+    com_size += sizeof(variant_43_trailer);
+    com_size_read += sizeof(variant_43_trailer);
+    variant = 0;
+  }
+  if (write(outfd, com_start, com_size_read) != com_size_read) goto write_error;
+  com_size -= com_size_read;
+  while (com_size > 0) {
+    int attempt = (com_size > 16384) ? 16384 : com_size;
+    if (read(infd, buf, attempt) != attempt) goto read_error;
+    com_size -= attempt;
+    if (com_size == 0 && variant == 43) {
+      memcpy(buf + (uint16_t)attempt, variant_43_trailer, sizeof(variant_43_trailer));
+      attempt += sizeof(variant_43_trailer);
+    }
+    if (write(outfd, buf, attempt) != attempt) goto write_error;
+  }
+  close(outfd);
+  close(infd);
   return 0;  /* EXIT_SUCCESS. */
 }
